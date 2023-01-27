ca292a1a50cea276c505e65dab3e33a2622ebc9f
Quinn Le <quynhnle135@gmail.com>
2023-01-27 13:24:32 -0500

    Make a madlib program in Python and learn about string concatenation

---
+++ madlibs/string_concatenation.py
@@ -0,0 +1,13 @@
+youtuber = "Joma Oppa"
+
+# here are three ways to concatenate string
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
+
+print("-------")
+
+name = "Quinn"
+print(name + " is making a new project")
+print("{} is making a new project".format(name))
+print(f"{name} is making a new project")
